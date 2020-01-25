e47fc7b108e9de167b3f501c80f5acbc3ad00d26
Andrew McMechan <coderwurst@gmail.com>
2020-01-25 21:57:09 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://github.com/coderwurst/termin8"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Termin8r
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
