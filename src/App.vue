136fc971befca2e2be638854aaa10e0da5a7e09d
Andrew McMechan <coderwurst@gmail.com>
2020-02-17 20:57:38 +0100

    first contact with backend

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Calender msg="Welcome to Termin8r" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calender from "./components/Calender.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Calender
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/Calender.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h2>Your Calender</h2>
+    <p>{{ meetings }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Calender",
+  data() {
+    return {
+      meetings: null
+    };
+  },
+  created: function() {
+    axios.get("http://localhost:8080/api/meeting/list/12345").then(response => {
+      this.meetings = response.data;
+    });
+  }
+};
+</script>
+
+<style>
+h3 {
+  margin-bottom: 5%;
+}
+</style>
